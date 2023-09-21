912a9254c85f9c7dbce5e5c8a39efb36bdcc1ce6
Pataki Balázs <patakib22@ADY-NAGYATAD.SULINET.HU>
2023-09-21 13:17:23 +0200

    asdsadskjmf

--- rangee.py
+++ rangee.py
@@ -1,5 +1,28 @@
 i=5
 
-for i in range(10,100,):
-    print(i, end="\t")
+for i in range(100):
+    if i %10==0:
+        print(i)
+
+for i in range(10):
+    print(i*i)
+
+for i in range(0,100):
+    if 100 <= i*i:
+        if 999 >=i*i:
+            print(i*i)
+
+for i in range(1,11):
+        print(i*2)
+
+for i in range(99,201):
+        print(i//5)
+            
+    
+    
+
+
+
+
+
     
