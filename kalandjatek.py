746d06762a5579ccdd8d7e8eacf73422329f6d8f
pbalazs22 <120101312+pbalazs22@users.noreply.github.com>
2023-11-17 22:35:53 +0100

    Add files via upload

--- kalandjatek.py
+++ kalandjatek.py
@@ -5,44 +5,50 @@ def menu(lista):
     valasz=-1
     while (valasz<1 or valasz>len(lista)) and valasz!=999:
         try:
-            valasz=int(input("Válassz: "))
+            valasz=int(input("Válasz: "))
         except:
             pass
     return valasz-1
 
 
-lista=["előre","hátra","jobbra","balra"]
-
 #valasztott=menu(lista)
 #print(valasztott,lista[valasztott])
 
 #valasztott=menu(lista)
 #print(valasztott,lista[valasztott])
 
-tortenet=[
-    [
-        1,  #történetszál sorszáma
-        "Reggel felkeltem.",#történetszál
-        ["fogmosás","reggeli", "öltözés"],#választható cselekvések
-        [2,3,4]#cselekvés sorszáma, hova ugorjon
-    ],
-    [
-        2,
-        "Megmostam a fogam, mert még van!",
-        ["fogmosás","reggeli", "öltözés"],#választható cselekvések
-        [2,3,4]#cselekvés sorszáma, hova ugorjon
-    ]
-]
+nyelvek=["Magyar","English"]
+nyelvId=menu(nyelvek)
+
+if nyelvId==0:
+    import tortenetmagyar as t
+elif nyelvId==1:
+    import tortenetangol as t
+
 szalId=1
 while True:
     tempLista=[]
-    for elem in tortenet:
+    for elem in t.tortenet:
         tempLista.append(elem[0])
 
     keresettIndex=tempLista.index(szalId)
-    print(tortenet[keresettIndex][1])
-    valasztott=menu(tortenet[keresettIndex][2])
+    print(t.tortenet[keresettIndex][1])
+    valasztott=menu(t.tortenet[keresettIndex][2])
 
-    szalId=tortenet[keresettIndex][3][valasztott]
+    szalId=t.tortenet[keresettIndex][3][valasztott]
     if valasztott==999:
         break
+
+    keresettIndex=tempLista.index(szalId)
+
+    print(t.tortenet[keresettIndex][1])
+    valasztott=menu(t.tortenet[keresettIndex][2])
+
+    szalId=t.tortenet[keresettIndex][3][valasztott]
+    if valasztott==999:
+        break
+
+
+
+
+
