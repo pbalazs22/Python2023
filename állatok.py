5dcefc353898b5763aadc8827d8c07f975c422b3
Pataki Balázs <patakib22@ADY-NAGYATAD.SULINET.HU>
2023-11-21 13:20:16 +0100

    jnoihgg

--- állatok.py
+++ állatok.py
@@ -1,12 +1,10 @@
 def legkonnyebb():
-    legkissebb=tomegek[0]
-    fort t in tomegek:
-        if tomeg<t:
-            t=tomeg
+    legkisebb=tomegek[0]
+    for t in tomegek:
+        if legkisebb>t:
+            legkisebb=t
 
-    pass
-
-
+    return legkisebb
 
 
 #egy lista amibe 10 állat van, készitsünk egy programot ami bekéri ezeknek az állatoknak a tömegét, ezt is menztse le , aztán egy függvény ami megkeresi a legkisseb tömegü állatot és visszaadja az adatait 
@@ -14,9 +12,20 @@ def legkonnyebb():
 allatok=["majom","kenguru","csincsilla","elefánt","vaddisznó","kutya","ló","macska","cápa","gepárd"]
 
 tomegek=[]
+    
+tomegek=[12,2,3,4,5,6,7,8,9,10]
 
-for allat in allatok:
-    tomegek.append(float(input("Milyen nehéz a "+allat+"(kg):")))
+#for allat in allatok:
+#   tomegek.append(float(input("Milyen nehéz a "+allat+"(kg):")))
 
 
-legkonnyebb()
+print(legkonnyebb())
+
+if legkonnyebb() in tomegek:
+    id=tomegek.index(legkonnyebb())
+    print(allatok[id])
+
+nehezek=[]
+
+for i in range(len(tomegek)):
+    
